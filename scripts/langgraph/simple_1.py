4f65b2d05edd25a650e2e385743173d3fb1e7603
Frizlen, Yasmine J <yf25511@essex.ac.uk>
2026-05-13 08:23:28 +0100

    16172 langgraph installation

---
+++ scripts/langgraph/simple_1.py
@@ -0,0 +1,35 @@
+
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict
+import subprocess
+
+class State(TypedDict):
+    message: str
+
+def main():
+    workflow = StateGraph(State)
+
+    workflow.add_node("node_1", node1)
+    workflow.add_node("node_2", node2)
+
+    workflow.add_edge(START, "node_1")
+    workflow.add_edge("node_1", "node_2")
+    workflow.add_edge("node_2", END)
+
+    app = workflow.compile()
+
+    result = app.invoke({"message": "hello"})
+    print(result)
+
+    with open("graph.png", "wb") as f:
+        f.write(app.get_graph().draw_mermaid_png())
+    subprocess.run(["open", "graph.png"])  # macOS: opens in Preview
+
+def node1(state: State) -> State:
+    return {"message": state["message"] + " I reached Node1."}
+
+def node2(state: State) -> State:
+    return {"message": state["message"] + " I reached Node2."}
+
+if __name__ == "__main__":
+    main()
